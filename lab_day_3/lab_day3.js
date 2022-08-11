37b56944dbf8726bf883f4b544ee804443cdb21c
Charlynpolanco <110350236+Charlynpolanco@users.noreply.github.com>
2022-08-10 20:53:39 -0400

    Add files via upload

---
+++ lab_day_3/lab_day3.js
@@ -0,0 +1,49 @@
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+      console.log(i)
+}
+}
+let grade = 70
+
+if (grade >= 80){
+    console.log("good job")
+}
+    else if(grade >= 70){
+        console.log("keep trying")
+    }
+    else if(grade >= 60){
+        console.log("ehhhh")
+    }
+    else if(grade >= 55){
+        console.log("Not too good")
+    } 
+    else{
+        console.log("Any grade lower than 55 is Bad grade But You'll get em next time")
+    }
+        
+for (let i = 10; i <= 90; i+=20) {
+    console.log(i)
+    }
+    for (let i = 20; i <= 100; i+=20) {
+        console.log(i)
+        }
+
+
+    
+    
+for (let i = 1; i <= 100; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz")
+    }
+    else if (i % 3 === 0){
+        console.log("Fizz")
+    }
+    else if (i % 5 === 0)
+        console.log("Buzz")
+    else{
+        console.log(i)
+    }
+
+            
+        
+}
